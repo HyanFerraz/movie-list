4657277f042a217242e38ef30d12f47d48b842db
Hyan Ferraz <hyanfr01@gmail.com>
2024-04-29 18:30:32 -0300

    refactor: remove unused imports and endpoint "me"

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,22 +1,16 @@
 import {
   Body,
   Controller,
-  Get,
-  HttpCode,
-  HttpStatus,
   Post,
   Req,
-  Res,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { LocalGuard } from './guard/local.guard';
-import { Request, Response } from 'express';
+import { Request } from 'express';
 import { JwtInterceptor } from './interceptors/jwt.interceptor';
-import { AuthGuard } from '@nestjs/passport';
-import { JwtGuard } from './guard/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -30,13 +24,8 @@ export class AuthController {
   }
 
   @Post('register')
+  @UseInterceptors(JwtInterceptor)
   async register(@Body() payload: AuthDto) {
     return await this.authService.createUser(payload);
-  }
-
-  @Get('me')
-  @UseGuards(JwtGuard)
-  async profile(@Req() req: Request) {
-    return req.user;
   }
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocalStrategy } from './strategies/local.strategy';
-import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
